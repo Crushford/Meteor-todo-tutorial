696a85049e2322bd77fc6d56db5c98372da7e559
James Rushford <Crushford@users.noreply.github.com>
2019-05-08 17:58:19 +0200

    filter for uncompleted tasks and uncompleted count
    -added option to filter out completed tasks
    -added count of tasks to be completed

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,28 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
  
 import './body.html';
 import './task.js';
+
+
+Template.body.onCreated(function bodyOnCreated(){
+    this.state = new ReactiveDict();
+})
  
 Template.body.helpers({
 tasks(){
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')){
+        //if hide completed is checked, filter tasks
+        return Tasks.find( {checked: {$ne:true } }, {sort: {createdAt:-1} });
+    }
+    //else return all tasks
     return Tasks.find({}, {sort: {createdAt:-1} });
+    },
+    incompleteCount(){
+        return Tasks.find({ checked: { $ne:true } }).count();
     },
 });
 
@@ -30,5 +45,8 @@ Template.body.events({
 
         //clear form
         target.text.value = '';
+    },
+    "change .hide-completed input"(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked)
     },
 });
