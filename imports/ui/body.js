3d3a30a48d8f9c77b93cb70fc01400e7cef95434
James Rushford <Crushford@users.noreply.github.com>
2019-05-08 16:16:46 +0200

    Task input added
    -added ability to creat new task from client
    -change list of tasks to be soted newest to oldest

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,28 @@ import './body.html';
  
 Template.body.helpers({
 tasks(){
-    return Tasks.find({});
+    return Tasks.find({}, {sort: {createdAt:-1} });
+    },
+});
+
+Template.body.events({
+    'submit .new-task'(event){
+        //prevent default broswer form submmit
+        event.preventDefault();
+
+        // console.log(event);
+
+        //get value from form
+        const target = event.target;
+        const text = target.text.value;
+
+        //insert a task into the colelction
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        //clear form
+        target.text.value = '';
     },
 });
